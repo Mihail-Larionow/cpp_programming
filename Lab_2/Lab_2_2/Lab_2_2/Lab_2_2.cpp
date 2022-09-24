71e5d0765f25a2c426ecff2203b24d23d83365e9
michel <mihail.larionow2001@gmail.com>
2022-09-24 16:56:40 +0300

    Lab_2_2 is done

---
+++ Lab_2/Lab_2_2/Lab_2_2/Lab_2_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+//Функция определения является ли номер валдиным
+bool IsValid(std::string number)
+{
+    for (int i = 2; i < 9; i++)
+        if (number[i] == number[i - 1] && number[i] == number[i - 2])
+            return false;
+
+
+}
+
+
+int main()
+{
+    system("chcp 1251"); //Устанавливаем кодировку
+    std::string number;
+
+    std::cout << "Пожалуйста, введите номер СНИЛСа:\n";
+
+    std::cin >> number;
+
+    if (IsValid(number)) std::cout << "Номер валидный!\n";
+    else std::cout << "Номер не валидный!\n";
+}

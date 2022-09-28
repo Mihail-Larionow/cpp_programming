18a1a724640e307e1a95c1085e513cce57260be6
michel <mihail.larionow2001@gmail.com>
2022-09-28 19:05:15 +0300

    Lab_3_4 and Lab_3_5 are done

---
+++ Lab_3/Lab_3_4/Lab_3_4/Lab_3_4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+//Рекурсивная функция суммы ряда
+int F(int n) {
+    if (n > 1) return 5 * n + F(n - 1);
+    else return 5;
+    
+}
+
+int main()
+{
+    system("chcp 1251"); //Устанавливаем кодировку
+    int n;
+
+    std::cout << "Пожалуйста, введите n:\n";
+    std::cin >> n; 
+
+    std::cout << "F(" << n << ") = " << F(n) << "\n"; //Вывод
+}
---
+++ Lab_3/Lab_3_5/Lab_3_5/Lab_3_5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+//Рекурсивная функция перевода целого числа в двоичный код
+std::string F(int n) {
+    if (n > 0) return F(n / 2) + std::to_string(n % 2) ;
+    else return "";
+}
+
+int main()
+{
+    system("chcp 1251"); //Устанавливаем кодировку
+    int n;
+
+    std::cout << "Пожалуйста, введите n:\n";
+    std::cin >> n;
+
+    std::cout << "F(" << n << ") = " << F(n) << "\n"; //Вывод
+}

6aa4cb730cf7a652265d06db85a51448e402ad1a
michel <mihail.larionow2001@gmail.com>
2022-10-13 00:49:04 +0300

    Lab_4 is done

---
+++ Lab_4/Lab_4_1/Lab_4_1/Lab_4_1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+//Функция нахождения корней
+int MyRoot(double a, double b, double c, double &x1, double &x2) {
+    double D = b * b - 4 * a * c;
+    if (D > 0) {
+        x1 = (-b + sqrt(D)) / (2 * a);
+        x2 = (-b - sqrt(D)) / (2 * a);
+        return 1;
+    }
+    else if (D == 0) {
+        x1 = (-b) / (2 * a);
+        x2 = (-b) / (2 * a);
+        return 0;
+    }
+    else return 0;
+}
+
+int main()
+{
+    system("chcp 1251"); //Устанавливаем кодировку
+    double x1, x2, a, b, c;
+
+    std::cout << "Пожалуйста, введите коэффициенты a, b и c уравнения вида ax^2+bx+c:\n";
+    std::cin >> a >> b >> c;
+
+    int answer = MyRoot(a, b, c, x1, x2);
+    if (answer == 1) std::cout << "Корни уравнения x1 = " << x1 << ", x2 = " << x2;
+    else if (answer == 0) std::cout << "Корни уравнения x1 = x2 = " << x1;
+    else std::cout << "Корней уравнения нет";
+}
---
+++ Lab_4/Lab_4_2/Lab_4_2/Lab_4_2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+//Функция Input
+bool Input(int &a, int &b) {
+    std::string s1, s2;
+    std::cin >> s1 >> s2;
+    
+    //Обработчик ошибок
+    try
+    {
+        a = std::stoi(s1); //Перевод строки в целочисленный тип
+        b = std::stoi(s2); //Перевод строки в целочисленный тип
+    }
+    catch (const std::exception& ex)
+    {
+        return false;
+    }
+    
+    return true;
+}
+
+int main()
+{
+    system("chcp 1251"); //Устанавливаем кодировку
+    
+    int a, b;
+
+    std::cout << "Пожалуйста, введите два целых числа: ";
+
+    if (Input(a, b) == false) // if(!Input(a,b))
+    {
+        std::cerr << "error";
+        return 1;
+    }
+
+    int s = a + b;
+    std::cout << "Их сумма равна: " << s;
+}

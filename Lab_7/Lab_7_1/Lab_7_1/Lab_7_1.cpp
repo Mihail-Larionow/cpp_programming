43384507bcb1a1230ffbc1c1c1218b3373387070
michel <mihail.larionow2001@gmail.com>
2022-10-26 01:17:35 +0300

    Lab_8 is done

--- Lab_7/Lab_7_1/Lab_7_1/Lab_7_1.cpp
+++ Lab_7/Lab_7_1/Lab_7_1/Lab_7_1.cpp
@@ -17,14 +17,13 @@ Time SumTime(Time t1, Time t2) {
     Time t;
     t.seconds = (t1.seconds + t2.seconds) % 60;
     t.minutes = (t1.minutes + t2.minutes + (t1.seconds + t2.seconds) / 60) % 60;
-    t.hours = t1.hours + t2.hours + (t1.minutes + t2.minutes + (t1.seconds + t2.seconds) / 60) / 60;
+    t.hours = (t1.hours + t2.hours + (t1.minutes + t2.minutes + (t1.seconds + t2.seconds) / 60) / 60) % 24;
     return t;
 }
 
 //Функция разности интервалов времени
 Time SubTime(Time t1, Time t2) {
     Time t;
-    
     t.seconds = t1.seconds - t2.seconds;
     t.minutes = t1.minutes - t2.minutes;
     if (t.seconds < 0) {
@@ -36,6 +35,7 @@ Time SubTime(Time t1, Time t2) {
         t.hours -= (abs(t.minutes) / 60 + 1);
         t.minutes = 60 + t.minutes % 60;
     }
+    if(t.hours < 0) t.hours = 24 + t.hours % 24;
     return t;
 }
 
@@ -45,23 +45,17 @@ int main()
 
     Time time1, time2, time;
 
-    std::cout << "Введите, пожалуйста, время в формате часы, минуты, секунды: \n";
+    std::cout << "Введите, пожалуйста, время в формате часы, минуты, секунды:\n";
     std::cin >> time1.hours >> time1.minutes >> time1.seconds;
-    std::cout << "Введите, пожалуйста, еще время: \n";
+    std::cout << "Введите, пожалуйста, еще время:\n";
     std::cin >> time2.hours >> time2.minutes >> time2.seconds;
     std::cout << "\n";
     std::cout << "time1: " << time1.DataTime() << " секунд\n";
     std::cout << "time2: " << time2.DataTime() << " секунд\n\n";
     time = SumTime(time1, time2);
     std::cout << "Прибавляем time2 к time1:\nчасов: " << time.hours << "\nминут: " << time.minutes << "\nсекунд: " << time.seconds << "\n\n";
-    if (time1.DataTime() >= time2.DataTime()) {
-        time = SubTime(time1, time2);
-        std::cout << "Вычитаем time2 из time1:\nчасов: " << time.hours << "\nминут: " << time.minutes << "\nсекунд: " << time.seconds << "\n\n";
-    }
-    else{
-        time = SubTime(time2, time1);
-        std::cout << "Вычитаем time1 из time2:\nчасов: " << time.hours << "\nминут: " << time.minutes << "\nсекунд: " << time.seconds << "\n\n";
-    }
+    time = SubTime(time1, time2);
+    std::cout << "Вычитаем time2 из time1:\nчасов: " << time.hours << "\nминут: " << time.minutes << "\nсекунд: " << time.seconds << "\n\n";
 
     return 0;
 }

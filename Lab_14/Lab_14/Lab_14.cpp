7c1b146dd5a277cd43216a67acf8faa4f0df1b93
michel <mihail.larionow2001@gmail.com>
2022-12-06 18:30:20 +0300

    Finally all done

---
+++ Lab_14/Lab_14/Lab_14.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+//Шаблон фунеции, возвращающей среднее арифметическое всех элементов массива  
+template<class T>
+double avg(T arr[], int size) {
+	int sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += arr[i];
+	}
+	return (double) sum / size;
+}
+
+int main()
+{
+	int int_arr[] = { 9,3,17,6,5,4,31,2,12 }; //Массив целых чисел
+	double double_arr[] = { 2.1, 2.3,1.7,6.6,5.3,2.44,3.1,2.4,1.2 }; //Массив вещественных чисел
+	char char_arr[] = "Hello, word"; //Массив символов
+
+	std::cout << "int_arr = " << avg(int_arr, sizeof(int_arr) / sizeof(int_arr[0])) << "\n";
+	std::cout << "double_arr = " << avg(double_arr, sizeof(double_arr) / sizeof(double_arr[0])) << "\n";
+	std::cout << "char_arr = " << avg(char_arr, sizeof(char_arr) / sizeof(char_arr[0])) << "\n";
+}
+
---
+++ Lab_15/Lab_15/Lab_15.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <map>
+
+//Структура StudentGrade
+struct StudentGrade {
+    std::string name;
+    int grade;
+
+    StudentGrade(std::string studName, int studGrade) {
+        name = studName;
+        grade = studGrade;
+    }
+    
+};
+
+
+int main()
+{
+    system("chcp 1251"); //Устанавливаем кодировку
+
+    std::map <std::string, int> group;
+    int n;
+
+    std::cout << "Введите количество студентов, оценки которых Вы хотите добавить:\n";
+    std::cin >> n;
+    
+    std::cout << "\nВводите данные в виде: имя_студента оценка\n";
+    for (int i = 0; i < n; i++) {
+        std::string name;
+        int grade;
+        std::cin >> name >> grade;
+        StudentGrade stud = StudentGrade(name, grade);
+        group[stud.name] = stud.grade;
+    }
+
+    std::cout << "\nИтоговый список:\n";
+    for (const auto& stud : group) {
+        std::cout << stud.first << " " << stud.second << "\n";
+    }
+}
